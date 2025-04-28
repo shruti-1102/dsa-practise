263d2d80b465f71f945c4ae7ced7c1c73dc396b1
shruti <shrutisinghparihar1102@gmail.com>
2025-04-28 23:28:33 +0530

    some sorted rotated array questions

--- arrays.java
+++ arrays.java
@@ -14,9 +14,62 @@ public class arrays {
         }
         return -1;
     }
+
+    public static int minInArray (int nums[]) {
+        int low = 0;
+        int high = nums.length-1;
+        while (low<high) {
+            int mid = (low + high)/2;
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+
+    public static int maxInArray (int nums[]) {
+        int low = 0;
+        int high = nums.length-1;
+        while (low < high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] <  nums[high]) {
+                high = mid - 1;
+            } else {
+                low = mid;
+            }
+        }
+        return nums[high];
+    }
+
+    public static boolean findTarget (int nums[], int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while (low <= high) {
+            int mid = (low + high)/2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
     public static void main (String[] args) {
-        int nums[] = {-5, -2, 0, 3, 4, 7, 10};
-        int target = 9;
-        System.out.println(binSearch(nums, target));
+        //using basic binary search
+        // int nums[] = {-5, -2, 0, 3, 4, 7, 10};
+        // int target = 9;
+        // System.out.println(binSearch(nums, target));
+
+        //finding the minimum element in the rotated sorted array
+        int nums[] = {4, 5, 6, 7, 0, 1, 2};
+        int target = 4;
+        // System.out.println(minInArray(nums));
+        // System.out.println(maxInArray(nums));
+        System.out.println(findTarget(nums, target));
     }
 }
