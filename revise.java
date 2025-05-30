3c76979a1d9734ed03d5258353bc9966379932fe
shruti <shrutisinghparihar1102@gmail.com>
2025-05-30 17:39:11 +0530

    revise file changed

--- revise.java
+++ revise.java
@@ -177,8 +177,6 @@ public class revise {
             printArr(arr);
         }
     }
-    
-
     public static void printArr(int arr[]) {
         for (int i = 0; i<arr.length; i++) {
             System.out.print(arr[i] + " ");
